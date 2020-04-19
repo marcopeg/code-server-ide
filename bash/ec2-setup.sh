cbe20c6f42d0780a714bf226734fde3b08fb7816
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-04-19 09:11:15 +0000

    ping

--- bash/ec2-setup.sh
+++ bash/ec2-setup.sh
@@ -3,7 +3,16 @@ apt-get update -y
 apt-get install jq apache2-utils -y
 
 VSCODE_ROOT=/home/ubuntu/vscode-ide
+
+# Generate random access credentials
+#VSCODE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
+VSCODE_USERNAME=foo
+VSCODE_PASSWORD=bar
+
+# Generate the install log
 touch ${VSCODE_ROOT}/install.log
+echo "USERNAME: ${VSCODE_USERNAME}" >> ${VSCODE_ROOT}/install.log
+echo "PASSWORD: ${VSCODE_PASSWORD}" >> ${VSCODE_ROOT}/install.log
 
 
 #
@@ -16,7 +25,7 @@ apt-get update -y
 apt-cache policy docker-ce
 apt-get install -y docker-ce
 usermod -aG docker ubuntu
-echo "[OK]\n\n" >> ${VSCODE_ROOT}/install.log
+echo "[OK]" >> ${VSCODE_ROOT}/install.log
 
 
 
@@ -27,7 +36,7 @@ echo "Install Docker Compose..." >> ${VSCODE_ROOT}/install.log
 DOCKER_COMPOSE_VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
 curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x /usr/local/bin/docker-compose
-echo "[OK]\n\n" >> ${VSCODE_ROOT}/install.log
+echo "[OK]" >> ${VSCODE_ROOT}/install.log
 
 
 
@@ -37,7 +46,7 @@ echo "[OK]\n\n" >> ${VSCODE_ROOT}/install.log
 echo "Install HumbleCLI..." >> ${VSCODE_ROOT}/install.log
 git clone https://github.com/marcopeg/humble-cli.git /home/ubuntu/.humble-cli
 ln -s /home/ubuntu/.humble-cli/bin/humble.sh /usr/local/bin/humble
-echo "[OK]\n\n" >> ${VSCODE_ROOT}/install.log
+echo "[OK]" >> ${VSCODE_ROOT}/install.log
 
 
 
@@ -49,26 +58,19 @@ VSCODE_VERSION=$(curl --silent https://api.github.com/repos/cdr/code-server/rele
 VSCODE_SRC=${VSCODE_ROOT}/code-server
 VSCODE_DATA=/var/lib/code-server
 
-VSCODE_PASSWD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
-echo "password: ${VSCODE_PASSWD}" >> ${VSCODE_ROOT}/install.log
-
-# Save generated password to a local file
-# touch ${VSCODE_ROOT}/password-vscode
-# echo ${VSCODE_PASSWD} >> ${VSCODE_ROOT}/password-vscode
-
 # Ensure the directory for the source files exists
 mkdir -p ${VSCODE_SRC}
 
 # Download & Extract
 if [ ! -d "${VSCODE_SRC}/code-server-${VSCODE_VERSION}-linux-x86_64" ]; then
-  echo "Install Docker [download sources]..." >> ${VSCODE_ROOT}/install.log
+  echo "Install Code Server [download sources]..." >> ${VSCODE_ROOT}/install.log
   wget https://github.com/cdr/code-server/releases/download/${VSCODE_VERSION}/code-server-${VSCODE_VERSION}-linux-x86_64.tar.gz -P ${VSCODE_SRC}
   tar -xzvf ${VSCODE_SRC}/code-server-${VSCODE_VERSION}-linux-x86_64.tar.gz --directory ${VSCODE_SRC}
 fi
 
 # Install command from the downloaded version
 # cd code-server-${VSCODE_VERSION}-linux-x86_64
-echo "Install Docker [copy files]..." >> ${VSCODE_ROOT}/install.log
+echo "Install Code Server [copy files]..." >> ${VSCODE_ROOT}/install.log
 rm -f /usr/bin/code-server
 rm -rf /usr/lib/code-server
 cp -R ${VSCODE_SRC}/code-server-${VSCODE_VERSION}-linux-x86_64 /usr/lib/code-server
@@ -81,7 +83,7 @@ if [ ! -f "${VSCODE_DATA}" ]; then
 fi
 
 # Replace the service file
-echo "Install Docker [install service]..." >> ${VSCODE_ROOT}/install.log
+echo "Install Code Server [install service]..." >> ${VSCODE_ROOT}/install.log
 rm -f /lib/systemd/system/code-server.service
 tee -a /lib/systemd/system/code-server.service > /dev/null <<EOT
 [Unit]
@@ -90,7 +92,7 @@ After=nginx.service
 
 [Service]
 Type=simple
-Environment=PASSWORD=${VSCODE_PASSWD}
+Environment=PASSWORD=${VSCODE_PASSWORD}
 ExecStart=/usr/bin/code-server --host 0.0.0.0 --user-data-dir ${VSCODE_DATA} --auth password
 Restart=always
 
@@ -98,7 +100,7 @@ Restart=always
 WantedBy=multi-user.target
 EOT
 systemctl daemon-reload
-echo "[OK]\n\n" >> ${VSCODE_ROOT}/install.log
+echo "[OK]" >> ${VSCODE_ROOT}/install.log
 
 
 #
@@ -106,19 +108,13 @@ echo "[OK]\n\n" >> ${VSCODE_ROOT}/install.log
 #
 echo "Setup traefik data..." >> ${VSCODE_ROOT}/install.log
 TRAEFIK_DATA=/var/lib/traefik
-TRAEFIK_PASSWD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
-echo "password: ${TRAEFIK_PASSWD}" >> ${VSCODE_ROOT}/install.log
-
-# Save generated password to a local file
-# touch ${VSCODE_ROOT}/password-traefik
-# echo ${TRAEFIK_PASSWD} >> ${VSCODE_ROOT}/password-traefik
 
 # Ensure the directory for the source files exists
 mkdir -p ${TRAEFIK_DATA}
 
 # Generate the password into an htpasswd file for the ide
-htpasswd -b -c ${VSCODE_ROOT}/.htpasswd vscode ${TRAEFIK_PASSWD}
-echo "[OK]\n\n" >> ${VSCODE_ROOT}/install.log
+htpasswd -b -c ${VSCODE_ROOT}/.htpasswd ${VSCODE_USERNAME} ${VSCODE_PASSWORD}
+echo "[OK]" >> ${VSCODE_ROOT}/install.log
 
 
 #
@@ -131,9 +127,10 @@ echo "TRAEFIK_EMAIL=postmaster@gopigtail.com"  >> ${VSCODE_ROOT}/.env
 echo "TRAEFIK_DNS=proxy.t1.marcopeg.com" >> ${VSCODE_ROOT}/.env
 echo "VSCODE_DNS=code.t1.marcopeg.com" >> ${VSCODE_ROOT}/.env
 echo "VSCODE_ROOT=${VSCODE_ROOT}" >> ${VSCODE_ROOT}/.env
-echo "[OK]\n\n" >> ${VSCODE_ROOT}/install.log
+echo "[OK]" >> ${VSCODE_ROOT}/install.log
 
 echo "Pulling images..." >> ${VSCODE_ROOT}/install.log
 docker-compose -f ${VSCODE_ROOT}/docker-compose.yml pull
-echo "[OK]\n\n" >> ${VSCODE_ROOT}/install.log
+echo "[OK]" >> ${VSCODE_ROOT}/install.log
 
+echo "IDE Setup completed" >> ${VSCODE_ROOT}/install.log
