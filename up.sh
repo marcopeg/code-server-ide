8932ef17843a7579c20ed6d28c636eb6db539123
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-04-19 07:30:51 +0000

    ping

--- up.sh
+++ up.sh
@@ -1,3 +1,5 @@
 TRAEFIK_DATA=/var/lib/traefik \
+TRAEFIK_DNS=proxy.t1.marcopeg.com \
+VSCODE_DNS=code.t1.marcopeg.com \
   docker-compose -f /home/ubuntu/vscode-ide/compose/docker-compose.yml \
   up
