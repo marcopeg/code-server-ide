52b4715daab9555c1db32e6dda1cb15c040575e5
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-04-19 06:37:31 +0000

    Add boot scripts

---
+++ ec2-boot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git clone git@github.com:marcopeg/vscode-server-ide.git /home/ubuntu/vscode-server-ide
+/home/ubuntu/vscode-server-ide/ec2-setup.sh
---
+++ ec2-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+apt-get update -y
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update -y
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+usermod -aG docker ubuntu
+
+# Docker Compose (latest version)
+DOCKER_COMPOSE_VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
+curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)"
+chmod +x /usr/local/bin/docker-compose
+
+# Humble CLI (need to fix the -y compatibility)
+git clone https://github.com/marcopeg/humble-cli.git /home/ubuntu/.humble-cli
+ln -s /home/ubuntu/.humble-cli/bin/humble.sh /usr/local/bin/humble
