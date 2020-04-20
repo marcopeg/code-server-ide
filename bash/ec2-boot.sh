f3c81e8d78600bfe5fde7b90194d883fb9b1ae18
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-04-20 05:37:26 +0000

    ping

--- bash/ec2-boot.sh
+++ bash/ec2-boot.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-git clone https://github.com/marcopeg/vscode-server-ide.git /home/ubuntu/vscode-ide
-/home/ubuntu/vscode-ide/bash/ec2-setup.sh
+
+export VSCODE_CWD=/home/ubuntu/vscode-ide
+export VSCODE_PASSWORD=admin
+
+git clone https://github.com/marcopeg/vscode-server-ide.git ${VSCODE_CWD}
+${VSCODE_CWD}/bash/ec2-setup.sh
--- bash/ec2-setup.sh
+++ bash/ec2-setup.sh
@@ -2,62 +2,62 @@
 apt-get update -y
 apt-get install jq apache2-utils -y
 
-VSCODE_ROOT=/home/ubuntu/vscode-ide
 
+VSCODE_CWD=${VSCODE_CWD:-"/home/ubuntu/vscode-ide"}
 
 # Generate random access credentials
-#VSCODE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
-VSCODE_USERNAME=foo
-VSCODE_PASSWORD=bar
+VSCODE_USERNAME=${VSCODE_USERNAME:-admin}
+VSCODE_PASSWORD=${VSCODE_USERNAME:-$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)}
 
 # Generate the install log
-touch ${VSCODE_ROOT}/install.log
-echo "USERNAME: ${VSCODE_USERNAME}" >> ${VSCODE_ROOT}/install.log
-echo "PASSWORD: ${VSCODE_PASSWORD}" >> ${VSCODE_ROOT}/install.log
-echo "CWD: $(pwd)" >> ${VSCODE_ROOT}/install.log
+touch ${VSCODE_CWD}.log
+echo "IDE Basic Auth:" >> ${VSCODE_CWD}.log
+echo "USERNAME: ${VSCODE_USERNAME}" >> ${VSCODE_CWD}.log
+echo "PASSWORD: ${VSCODE_PASSWORD}" >> ${VSCODE_CWD}.log
+echo "CWD: $(pwd)" >> ${VSCODE_CWD}.log
 
 
 #
 # Install Docker
 #
-echo "Install Docker..." >> ${VSCODE_ROOT}/install.log
+echo "Install Docker..." >> ${VSCODE_CWD}.log
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
 apt-cache policy docker-ce
 apt-get install -y docker-ce
 usermod -aG docker ubuntu
-echo $'[OK]\n\n' >> ${VSCODE_ROOT}/install.log
+echo $'[OK]\n\n' >> ${VSCODE_CWD}.log
 
 
 
 #
 # Docker Compose (latest version)
 #
-echo "Install Docker Compose..." >> ${VSCODE_ROOT}/install.log
+echo "Install Docker Compose..." >> ${VSCODE_CWD}.log
 DOCKER_COMPOSE_VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
 curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x /usr/local/bin/docker-compose
-echo $'[OK]\n\n' >> ${VSCODE_ROOT}/install.log
+echo $'[OK]\n\n' >> ${VSCODE_CWD}.log
 
 
 
 #
 # Humble CLI (need to fix the -y compatibility)
 #
-echo "Install HumbleCLI..." >> ${VSCODE_ROOT}/install.log
+echo "Install HumbleCLI..." >> ${VSCODE_CWD}.log
 git clone https://github.com/marcopeg/humble-cli.git /home/ubuntu/.humble-cli
 ln -s /home/ubuntu/.humble-cli/bin/humble.sh /usr/local/bin/humble
-echo $'[OK]\n\n' >> ${VSCODE_ROOT}/install.log
+echo $'[OK]\n\n' >> ${VSCODE_CWD}.log
 
 
 
 #
 # Code Server
 #
-echo "Install Code Server..." >> ${VSCODE_ROOT}/install.log
+echo "Install Code Server..." >> ${VSCODE_CWD}.log
 VSCODE_VERSION=$(curl --silent https://api.github.com/repos/cdr/code-server/releases/latest | jq .name -r)
-VSCODE_SRC=${VSCODE_ROOT}/code-server
+VSCODE_SRC=${VSCODE_CWD}/code-server
 VSCODE_DATA=/var/lib/code-server
 
 # Ensure the directory for the source files exists
@@ -65,14 +65,14 @@ mkdir -p ${VSCODE_SRC}
 
 # Download & Extract
 if [ ! -d "${VSCODE_SRC}/code-server-${VSCODE_VERSION}-linux-x86_64" ]; then
-  echo "> download sources" >> ${VSCODE_ROOT}/install.log
+  echo "> download sources" >> ${VSCODE_CWD}.log
   wget https://github.com/cdr/code-server/releases/download/${VSCODE_VERSION}/code-server-${VSCODE_VERSION}-linux-x86_64.tar.gz -P ${VSCODE_SRC}
   tar -xzvf ${VSCODE_SRC}/code-server-${VSCODE_VERSION}-linux-x86_64.tar.gz --directory ${VSCODE_SRC}
 fi
 
 # Install command from the downloaded version
 # cd code-server-${VSCODE_VERSION}-linux-x86_64
-echo "> copy files" >> ${VSCODE_ROOT}/install.log
+echo "> copy files" >> ${VSCODE_CWD}.log
 rm -f /usr/bin/code-server
 rm -rf /usr/lib/code-server
 cp -R ${VSCODE_SRC}/code-server-${VSCODE_VERSION}-linux-x86_64 /usr/lib/code-server
@@ -85,7 +85,7 @@ if [ ! -f "${VSCODE_DATA}" ]; then
 fi
 
 # Replace the service file
-echo "> install service" >> ${VSCODE_ROOT}/install.log
+echo "> install service" >> ${VSCODE_CWD}.log
 rm -f /lib/systemd/system/code-server.service
 tee -a /lib/systemd/system/code-server.service > /dev/null <<EOT
 [Unit]
@@ -102,37 +102,37 @@ Restart=always
 WantedBy=multi-user.target
 EOT
 systemctl daemon-reload
-echo $'[OK]\n\n' >> ${VSCODE_ROOT}/install.log
+echo $'[OK]\n\n' >> ${VSCODE_CWD}.log
 
 
 #
 # Traefik & IDE Compose setup
 #
-echo "Setup traefik data..." >> ${VSCODE_ROOT}/install.log
+echo "Setup traefik data..." >> ${VSCODE_CWD}.log
 TRAEFIK_DATA=/var/lib/traefik
 
 # Ensure the directory for the source files exists
 mkdir -p ${TRAEFIK_DATA}
 
 # Generate the password into an htpasswd file for the ide
-htpasswd -b -c ${VSCODE_ROOT}/.htpasswd ${VSCODE_USERNAME} ${VSCODE_PASSWORD}
-echo $'[OK]\n\n' >> ${VSCODE_ROOT}/install.log
+htpasswd -b -c ${VSCODE_CWD}/.htpasswd ${VSCODE_USERNAME} ${VSCODE_PASSWORD}
+echo $'[OK]\n\n' >> ${VSCODE_CWD}.log
 
 
 #
 # Create .env file
 #
-echo "Create env file..." >> ${VSCODE_ROOT}/install.log
-touch ${VSCODE_ROOT}/.env
-echo "TRAEFIK_DATA=${TRAEFIK_DATA}" >> ${VSCODE_ROOT}/.env
-echo "TRAEFIK_EMAIL=postmaster@gopigtail.com"  >> ${VSCODE_ROOT}/.env
-echo "TRAEFIK_DNS=proxy.t1.marcopeg.com" >> ${VSCODE_ROOT}/.env
-echo "VSCODE_DNS=code.t1.marcopeg.com" >> ${VSCODE_ROOT}/.env
-echo "VSCODE_ROOT=${VSCODE_ROOT}" >> ${VSCODE_ROOT}/.env
-echo $'[OK]\n\n' >> ${VSCODE_ROOT}/install.log
+echo "Create env file..." >> ${VSCODE_CWD}.log
+touch ${VSCODE_CWD}/.env
+echo "TRAEFIK_DATA=${TRAEFIK_DATA}" >> ${VSCODE_CWD}/.env
+echo "TRAEFIK_EMAIL=postmaster@gopigtail.com"  >> ${VSCODE_CWD}/.env
+echo "TRAEFIK_DNS=proxy.t1.marcopeg.com" >> ${VSCODE_CWD}/.env
+echo "VSCODE_DNS=code.t1.marcopeg.com" >> ${VSCODE_CWD}/.env
+echo "VSCODE_CWD=${VSCODE_CWD}" >> ${VSCODE_CWD}/.env
+echo $'[OK]\n\n' >> ${VSCODE_CWD}.log
 
-echo "Pulling images..." >> ${VSCODE_ROOT}/install.log
-docker-compose -f ${VSCODE_ROOT}/docker-compose.yml pull
-echo $'[OK]\n\n' >> ${VSCODE_ROOT}/install.log
+echo "Pulling images..." >> ${VSCODE_CWD}.log
+docker-compose -f ${VSCODE_CWD}/docker-compose.yml pull
+echo $'[OK]\n\n' >> ${VSCODE_CWD}.log
 
-echo "IDE Setup completed" >> ${VSCODE_ROOT}/install.log
+echo "IDE Setup completed" >> ${VSCODE_CWD}.log
