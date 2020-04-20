c2e70b55fcb1db6414529c38707b51515331602a
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-04-20 06:26:03 +0000

    ping

--- bash/ec2-boot.sh
+++ bash/ec2-boot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Setup your IDE:
-export VSCODE_CWD=/home/ubuntu/vscode-ide
-export VSCODE_PASSWORD=admin
-export VSCODE_DNS=t3.marcopeg.com
+export VSCODE_DNS="t3.marcopeg.com"
+export VSCODE_PASSWORD="admin"
+export VSCODE_CWD="/home/ubuntu/vscode-ide"
 
 # Clone the repo and run the install script:
 git clone https://github.com/marcopeg/vscode-server-ide.git ${VSCODE_CWD}
