cdbd9658d7991187fde79a5b5bd62d1fd34913f5
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-04-24 15:21:22 +0000

    ping

--- ec2-ubuntu-cloudflare.sh
+++ ec2-ubuntu-cloudflare.sh
@@ -4,8 +4,8 @@
 
 # Load the environment variables
 set -o allexport
-#source "${CWD}/.env"
-source /etc/bash.bashrc
+source "${CWD}/.env"
+#source /etc/bash.bashrc
 set +o allexport
 
 # Takes action only if the API KEY is set:
