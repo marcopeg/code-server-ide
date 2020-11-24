fbd644f132a2c5eb6811fd5750d0f3c973e2eb6a
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-11-24 07:56:27 +0000

    Destroy

--- src/destroy.sh
+++ src/destroy.sh
@@ -4,6 +4,7 @@ source "$(dirname "$0")/profile.sh"
 
 # Remove the entire data folder
 rm -rf $CODE_SERVER_DATA
+rm -rf $CODE_SERVER_LOGS
 
 # Remove CodeServer service file
 rm -f /lib/systemd/system/code-server-ide.service
