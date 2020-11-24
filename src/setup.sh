35cc0501fdab989c18e142e2ca47078c6c37285c
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-11-24 07:41:05 +0000

    Fix path

--- src/setup.sh
+++ src/setup.sh
@@ -3,4 +3,4 @@
 source "$(dirname "$0")/profile.sh"
 source "$(dirname "$0")/setup-folders.sh"
 source "$(dirname "$0")/setup-logs.sh"
-source "$(dirname "$0")/apt-get.sh"
+source "$(dirname "$0")/setup-apt-get.sh"
