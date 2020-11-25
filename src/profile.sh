0d6e598f963f7c6a917915e749e52caab9ae882e
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-11-25 06:57:44 +0000

    Add ver

--- src/profile.sh
+++ src/profile.sh
@@ -4,6 +4,11 @@
 ### Defines all the variables that are needed to the code-server-ide project
 ###
 
+# Get OS major version
+# (Docker depends on this)
+CODE_SERVER_OSV=$(cut -f2 <<< $(lsb_release -r))
+export CODE_SERVER_OSV=${CODE_SERVER_OSV%.*}
+
 # CWD with default based on Ubuntu home folder
 export CODE_SERVER_CWD=${CODE_SERVER_CWD:-"/home/ubuntu/.code-server-ide"}
 export CODE_SERVER_DATA=${CODE_SERVER_CWD}/data/code-server
--- src/setup-docker.sh
+++ src/setup-docker.sh
@@ -6,38 +6,30 @@
 
 source "$(dirname "$0")/profile.sh"
 
-# Get OS major version
-# (Docker depends on this)
-OSV=$(cut -f2 <<< $(lsb_release -r))
-OSV=${OSV%.*}
+if [ "${CODE_SERVER_OSV}" -eq "20" ]
+then
+    echo "[$(date -u)] Install Docker for Ubuntu 20.x..." >> ${CODE_SERVER_LOG}
+    apt install -y docker.io
+    systemctl enable --now docker
+    usermod -aG docker ubuntu
+fi
 
-echo ${OSV}
-echo ${CODE_SERVER_LOG}
+if [ "${OSV}" -lt "20" ]
+then
+    echo "[$(date -u)] Install Docker for Ubuntu (16/18/19).x..." >> ${CODE_SERVER_LOG}
+    apt update -y
+    apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    echo "> update apt-get" >> ${VSCODE_LOG}
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    # add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt update -y
+    echo "> run install script" >> ${VSCODE_LOG}
+    apt -y install docker-ce docker-ce-cli containerd.io
+    # apt-cache policy docker-ce
+    # apt install docker-ce
+    usermod -aG docker ubuntu
+fi
 
-# if [ "${OSV}" -eq "20" ]
-# then
-#     echo "[$(date -u)] Install Docker for Ubuntu 20.x..." >> ${CODE_SERVER_LOG}
-#     apt install -y docker.io
-#     systemctl enable --now docker
-#     usermod -aG docker ubuntu
-# fi
-
-# if [ "${OSV}" -lt "20" ]
-# then
-#     echo "[$(date -u)] Install Docker for Ubuntu (16/18/19).x..." >> ${CODE_SERVER_LOG}
-#     apt update -y
-#     apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-#     echo "> update apt-get" >> ${VSCODE_LOG}
-#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-#     # add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-#     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#     apt update -y
-#     echo "> run install script" >> ${VSCODE_LOG}
-#     apt -y install docker-ce docker-ce-cli containerd.io
-#     # apt-cache policy docker-ce
-#     # apt install docker-ce
-#     usermod -aG docker ubuntu
-# fi
-
-# echo $'[OK]\n' >> ${CODE_SERVER_LOG}
+echo $'[OK]\n' >> ${CODE_SERVER_LOG}
 
