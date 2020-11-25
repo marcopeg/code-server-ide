255c7f5ef7da42f8c7558f2f8d1bfe7ca51a292a
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-11-25 08:26:23 +0000

    Update ubuntu

--- src/boot-ubuntu.sh
+++ src/boot-ubuntu.sh
@@ -14,8 +14,12 @@
 #############################################
 # DO NOT MODIFY THE CODE BELOW THIS COMMENT #
 #############################################
+
+# Add environment variables to the bash profile:
 echo $'\n\n# CODE SERVER IDE:' >> /home/ubuntu/.profile
 echo "export CODE_SERVER_CWD=${CODE_SERVER_CWD:-"/home/ubuntu/.code-server-ide"}" >> /home/ubuntu/.profile
+
+# Run the main setup script
 source /home/ubuntu/.profile
 git clone https://github.com/marcopeg/code-server-ide.git ${CODE_SERVER_CWD}
-${CODE_SERVER_CWD}/src/boot-ubuntu.sh
+${CODE_SERVER_CWD}/src/setup.sh
