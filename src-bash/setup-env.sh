299ffd32dfff8420e150c70139160ddfc7337482
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-11-26 08:10:32 +0000

    add env

--- src-bash/setup-env.sh
+++ src-bash/setup-env.sh
@@ -9,4 +9,9 @@ echo "[$(date -u)] Create env file..." >> ${CODE_SERVER_LOG}
 touch ${CODE_SERVER_CWD}/.env
 echo "# Code Server IDE Configuration" >> ${CODE_SERVER_CWD}/.env
 echo "CODE_SERVER_CWD=${CODE_SERVER_CWD}" >> ${CODE_SERVER_CWD}/.env
+echo "CODE_SERVER_DATA=${CODE_SERVER_DATA}" >> ${CODE_SERVER_CWD}/.env
+echo "CODE_SERVER_LOGS=${CODE_SERVER_LOGS}" >> ${CODE_SERVER_CWD}/.env
+echo "CODE_SERVER_TRAEFIK=${CODE_SERVER_TRAEFIK}" >> ${CODE_SERVER_CWD}/.env
+echo "CODE_SERVER_DNS=${CODE_SERVER_DNS}" >> ${CODE_SERVER_CWD}/.env
+echo "CODE_SERVER_EMAIL=${CODE_SERVER_EMAIL}" >> ${CODE_SERVER_CWD}/.env
 echo $'[OK]\n' >> ${CODE_SERVER_LOG}
