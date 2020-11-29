f71638a8341bcd58c25cceac8f2bfe7e566e43ea
Ubuntu <ubuntu@ip-172-31-37-107.eu-west-1.compute.internal>
2020-11-29 15:38:56 +0000

    NVM

--- src/setup-software.sh
+++ src/setup-software.sh
@@ -11,25 +11,26 @@ source "$(dirname "$0")/setup-profile.sh"
 
 
 echo "[$(date -u)] Installing NVM..." >> ${CODE_SERVER_LOGS}/setup.log
-
-ls /home >> ${CODE_SERVER_LOGS}/setup.log
-ls /home/ubuntu >> ${CODE_SERVER_LOGS}/setup.log
-mkdir -p /home/ubuntu/.nvm >> ${CODE_SERVER_LOGS}/setup.log
-
+# Configure the installation
 export NVM_VERSION=$(curl --silent https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq .name -r)
 export NVM_DIR=/home/ubuntu/.nvm
+# Running installation script
 echo "[$(date -u)] Version: ${NVM_VERSION}" >> ${CODE_SERVER_LOGS}/setup.log
+mkdir -p ${NVM_DIR} >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+touch /home/ubuntu/.profile >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash >> ${CODE_SERVER_LOGS}/setup.log 2>&1
-chmod -R ubuntu:ubuntu ${NVM_DIR}
+chown -R ubuntu:ubuntu ${NVM_DIR} >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+
 # Add the NVM bin stuff to the bash loading profile
-echo $'\n\n# NVM' >> /etc/bash.bashrc
-echo $'export NVM_DIR="/home/ubuntu/.nvm"' >> /etc/bash.bashrc
-echo $'[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/bash.bashrc
-echo $'[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> /etc/bash.bashrc
+# echo $'\n\n# NVM' >> /etc/bash.bashrc
+# echo $'export NVM_DIR="/home/ubuntu/.nvm"' >> /etc/bash.bashrc
+# echo $'[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/bash.bashrc
+# echo $'[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> /etc/bash.bashrc
 # Reload bash and test NVM
-source /etc/bash.bashrc
+source source ${NVM_DIR}/nvm.sh >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 nvm -v  >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 # Install latest Node LTS
-# nvm install --lts >> ${CODE_SERVER_LOGS}/setup.log 2>&1
-# node -v >> ${CODE_SERVER_LOGS}/setup.log 2>&1
-# npm -v >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+echo "[$(date -u)] Installing lastest NodeJS (LTS)..." >> ${CODE_SERVER_LOGS}/setup.log
+nvm install --lts >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+node -v >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+npm -v >> ${CODE_SERVER_LOGS}/setup.log 2>&1
