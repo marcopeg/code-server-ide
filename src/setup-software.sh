bf3e2d654e303b2f3b27884e3fe74773e4f506b9
Ubuntu <ubuntu@ip-172-31-37-107.eu-west-1.compute.internal>
2020-11-29 12:59:15 +0000

    NVM

--- src/setup-software.sh
+++ src/setup-software.sh
@@ -7,5 +7,9 @@ source "$(dirname "$0")/setup-profile.sh"
 ###
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash  >> ${CODE_SERVER_LOGS}/setup.log 2>&1
-source /home/ubuntu/.bashrc  >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+echo $'\n\n# NVM' >> /etc/bash.bashrc
+echo $'export NVM_DIR="$HOME/.nvm"' >> /etc/bash.bashrc
+echo $'[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/bash.bashrc
+echo $'[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> /etc/bash.bashrc
+source /etc/bash.bashrc  >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 nvm -v  >> ${CODE_SERVER_LOGS}/setup.log 2>&1
