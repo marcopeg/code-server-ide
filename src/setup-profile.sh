e63c26ab289ba4e2c03c70d0d71cd023b455b662
Ubuntu <ubuntu@ip-172-31-37-107.eu-west-1.compute.internal>
2020-11-29 18:35:42 +0000

    CWD

--- src/setup-profile.sh
+++ src/setup-profile.sh
@@ -4,17 +4,13 @@
 ### Defines all the variables that are needed to the code-server-ide project
 ###
 
-# Calculate the CWD:
-CWD=$(cd $(dirname "$0") && cd .. && pwd)
-echo "CWD=${CWD}"
-
 # Get OS major version
 # (Docker depends on this)
 CODE_SERVER_OSV=$(cut -f2 <<< $(lsb_release -r))
 export CODE_SERVER_OSV=${CODE_SERVER_OSV%.*}
 
 # CWD with default based on Ubuntu home folder
-export CODE_SERVER_CWD=${CODE_SERVER_CWD:-"${CWD}"}
+export CODE_SERVER_CWD=${CODE_SERVER_CWD:-"$(cd $(dirname "$0") && cd .. && pwd)"}
 echo "CODE_SERVER_CWD=${CODE_SERVER_CWD}"
 export CODE_SERVER_DATA=${CODE_SERVER_CWD}/data/code-server
 export CODE_SERVER_LOGS=${CODE_SERVER_CWD}/data/logs
--- src/setup.sh
+++ src/setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-CWD="$(dirname "$0")"
+CODE_SERVER_SETUP_SRC_CWD="$(dirname "$0")"
 
 # Setup:
-source "${CWD}/setup-init.sh"
-source "${CWD}/setup-apt-get.sh"
-source "${CWD}/setup-env.sh"
-source "${CWD}/setup-bashrc.sh"
-source "${CWD}/setup-software-code-server.sh"
-source "${CWD}/setup-software-docker.sh"
-source "${CWD}/setup-software-nvm.sh"
-source "${CWD}/setup-software-aws-cli.sh"
-source "${CWD}/setup-final.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-init.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-apt-get.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-env.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-bashrc.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-code-server.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-docker.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-nvm.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-aws-cli.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-final.sh"
 
 # First boot:
-source "${CWD}/ec2-ubuntu-boot.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/ec2-ubuntu-boot.sh"
