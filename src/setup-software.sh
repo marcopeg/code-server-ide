a474a6931baa52a0e72ba3e4f6683403ce6d44de
Ubuntu <ubuntu@ip-172-31-37-107.eu-west-1.compute.internal>
2020-11-29 15:47:23 +0000

    Add NVM and NodeJS LTS

--- src/setup-software.sh
+++ src/setup-software.sh
@@ -9,11 +9,11 @@ source "$(dirname "$0")/setup-profile.sh"
 ### yet available for the user.
 ###
 
-
 echo "[$(date -u)] Installing NVM..." >> ${CODE_SERVER_LOGS}/setup.log
 # Configure the installation
 export NVM_VERSION=$(curl --silent https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq .name -r)
 export NVM_DIR=/home/ubuntu/.nvm
+
 # Running installation script
 echo "[$(date -u)] Version: ${NVM_VERSION}" >> ${CODE_SERVER_LOGS}/setup.log
 mkdir -p ${NVM_DIR} >> ${CODE_SERVER_LOGS}/setup.log 2>&1
@@ -29,8 +29,9 @@ echo $'[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # Thi
 # Reload bash and test NVM
 source ${NVM_DIR}/nvm.sh >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 nvm -v  >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+
 # Install latest Node LTS
 echo "[$(date -u)] Installing lastest NodeJS (LTS)..." >> ${CODE_SERVER_LOGS}/setup.log
 nvm install --lts >> ${CODE_SERVER_LOGS}/setup.log 2>&1
-node -v >> ${CODE_SERVER_LOGS}/setup.log 2>&1
-npm -v >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+echo "NodeJS: $(node -v)" >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+echo "NPM: $(npm -v)" >> ${CODE_SERVER_LOGS}/setup.log 2>&1
