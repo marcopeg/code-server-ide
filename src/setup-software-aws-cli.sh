e273029ec668221add142d781d8e9709097a420a
Ubuntu <ubuntu@ip-172-31-37-107.eu-west-1.compute.internal>
2020-11-29 16:07:51 +0000

    AWS CLI

--- src/setup-software-aws-cli.sh
+++ src/setup-software-aws-cli.sh
@@ -10,7 +10,8 @@ echo "[$(date -u)] Installing AWS CLI v2..." >> ${CODE_SERVER_LOGS}/setup.log
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 unzip awscliv2.zip >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 ./aws/install >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+echo "AWS CLI $(aws --version)" >> ${CODE_SERVER_LOGS}/setup.log
 
-echo "AWS CLI ${aws --version}" >> ${CODE_SERVER_LOGS}/setup.log
-echo "Installed in ${pwd}" >> ${CODE_SERVER_LOGS}/setup.log
-echo "Installed in ${ls}" >> ${CODE_SERVER_LOGS}/setup.log
+# Cleanup
+rm -rf ./aws
+rm -f ./awscliv2.zip
