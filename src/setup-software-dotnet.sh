e0765271370ae0b8eed16de23d2fdbd3c3b16d26
Ubuntu <ubuntu@dev.marcopeg.com>
2020-12-13 06:50:02 +0000

    Fix .NET setup with SDK

--- src/setup-software-dotnet.sh
+++ src/setup-software-dotnet.sh
@@ -8,19 +8,20 @@ source "$(dirname "$0")/setup-profile.sh"
 
 echo "[$(date -u)] Install .NET 5.0 SDK & Framework for Ubuntu 20.x..." >> ${CODE_SERVER_LOGS}/setup.log
 
+echo "[$(date -u)] Adding .NET package..." >> ${CODE_SERVER_LOGS}/setup.log
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 dpkg -i packages-microsoft-prod.deb >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 rm -f packages-microsoft-prod.deb >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 
-apt-get update; \
-  apt-get install -y apt-transport-https && \
-  apt-get update && \
-  apt-get install -y dotnet-sdk-5.0 \
-  >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+echo "[$(date -u)] Adding .NET 5.0 SDK..." >> ${CODE_SERVER_LOGS}/setup.log
+apt-get update >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+apt-get install -y apt-transport-https >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+apt-get update >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+apt-get install -y dotnet-sdk-5.0 >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 
-apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y aspnetcore-runtime-5.0 \
-  >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+echo "[$(date -u)] Adding .NET 5.0 Runtime..." >> ${CODE_SERVER_LOGS}/setup.log
+apt-get update >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+apt-get install -y apt-transport-https >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+apt-get update >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+apt-get install -y aspnetcore-runtime-5.0 >> ${CODE_SERVER_LOGS}/setup.log 2>&1
 
