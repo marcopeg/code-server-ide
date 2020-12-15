b1c649909fa95a9a36385b85c56766075e3a795d
Ubuntu <ubuntu@dev.marcopeg.com>
2020-12-15 07:47:26 +0000

    Handle better change password
    closes #29

--- src/cs-pwd.sh
+++ src/cs-pwd.sh
@@ -9,41 +9,110 @@ set -o allexport
 source "${CWD}/.env"
 set +o allexport
 
-# Prepare the logs file
-touch ${CODE_SERVER_LOGS}/cs.log
-
-if [ -n "${1}" ]
-then
-    USER=${1}
-else
+function askUsrname() {
+  if [ -z ${CHANGE_USRNAME+x} ] ; then
     echo "Please enter the username you want to set/change the password for:"
-    read USER
-fi
+    read CHANGE_USRNAME
+  fi
 
-if [ ! -n "${USER}" ]
-then
-    echo "You must provide a valid username."
-    echo "Abort!"
+  if [ ! -n "${CHANGE_USRNAME}" ]
+  then
+    echo "ERROR: You must provide a valid username!"
     exit
-fi
+  fi
+}
 
-echo "Please enter the password:"
-read -s PASSWORD
+function askPasswd() {
+  if [ -z ${CHANGE_PASSWD+x} ] ; then
+    echo "Please enter the new password:"
+    read -s CHANGE_PASSWD
+    echo "Please re-enter the password (just to play safe):"
+    read -s CHANGE_PASSWD_CONFIRM
+    
+    if [ ${CHANGE_PASSWD} != ${CHANGE_PASSWD_CONFIRM} ]
+    then
+      echo "ERROR: Password mismatch!"
+      exit
+    fi
 
-echo "Please re-enter the password (just to play safe):"
-read -s PASSWORD1
+  fi
 
-if [ ${PASSWORD} != ${PASSWORD1} ]
-then
-    echo "Password mismatch!"
-    echo "Abort!"
+  if [ ! -n "${CHANGE_PASSWD}" ]
+  then
+    echo "ERROR: You must provide a valid password!"
     exit
+  fi
+}
+
+function ide() {
+  askPasswd
+
+  echo "Changing CodeServerIDE Password..."
+  echo "[$(date -u)] Changing IDE Password to: ${CHANGE_PASSWD}" >> ${CODE_SERVER_LOGS}/setup.log
+  echo ${CHANGE_PASSWD} > "${CODE_SERVER_CWD}/data/passwd"
+  docker-compose -f ${CODE_SERVER_CWD}/docker-compose.yml up -d --force-recreate auth-passwd >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+}
+
+function basicAuth() {
+  askUsrname
+  askPasswd
+
+  echo "Changing Basic Auth Password..."
+  echo "[$(date -u)] Changing Basic Auth Password for '${CHANGE_USRNAME}' to: ${CHANGE_PASSWD}" >> ${CODE_SERVER_LOGS}/setup.log
+  htpasswd -b ${CODE_SERVER_CWD}/data/.htpasswd ${CHANGE_USRNAME} ${CHANGE_PASSWD} >> ${CODE_SERVER_LOGS}/setup.log 2>&1
+  docker-compose -f ${CODE_SERVER_CWD}/docker-compose.yml up -d --force-recreate traefik >> ${CODE_SERVER_LOGS}/cs.log 2>&1
+}
+
+
+# Collect command line arguments:
+while [ "$#" -ne 0 ] ; do
+  case "$1" in
+    ide|basic-auth)
+      CMD="$1"
+      shift
+      ;;
+    -u|--user)
+      CHANGE_USRNAME="${2}"
+      shift 2
+      ;;
+    -p|--password)
+      CHANGE_PASSWD="${2}"
+      shift 2
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+# Present the main menu:
+if [ -z ${CMD+x} ]
+then
+  PS3='Please pick a choice:'
+  options=("Change IDE password" "Change Basic Auth password")
+  select opt in "${options[@]}"
+  do
+    case $opt in
+      "Change IDE password")
+        CMD=ide
+        break
+        ;;
+      "Change Basic Auth password")
+        CMD=basic-auth
+        break
+        ;;
+      *) echo "invalid option";;
+    esac
+  done
 fi
 
-echo "Changing password for user: ${USER}..."
-echo ">> Please count to 20, then reload the IDE"
-echo "[$(date -u)] Changing password for ${USER}" >> ${CODE_SERVER_LOGS}/cs.log
-echo "[$(date -u)] Simple Auth Username: ${USER}" >> ${CODE_SERVER_LOGS}/setup.log
-echo "[$(date -u)] Simple Auth Password: ${PASSWORD}" >> ${CODE_SERVER_LOGS}/setup.log
-htpasswd -b ${CODE_SERVER_CWD}/data/.htpasswd ${USER} ${PASSWORD}
-docker-compose -f ${CODE_SERVER_CWD}/docker-compose.yml up -d --force-recreate nginx
+case ${CMD} in
+    "ide")
+        ide ${@:2}
+        ;;
+    "basic-auth")
+        basicAuth ${@:2}
+        ;;
+    *) echo "invalid option";;
+esac
+
