6ce02b6d022906064b2e28e43c97304555a11a3d
Ubuntu <ubuntu@dev.marcopeg.com>
2020-12-16 20:20:48 +0000

    Boot

--- ansible/boot.sh
+++ ansible/boot.sh
@@ -3,8 +3,8 @@
 git clone -b dev/24 https://github.com/marcopeg/code-server-ide.git
 
 # Install Ansible
-apt update -y
-sudo apt install -y ansible
+apt update -y 
+apt install -y ansible
 
 # Install Dependent Rolesz
 ansible-galaxy install nickjj.docker
