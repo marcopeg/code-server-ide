29ff908855790858d70fee4b38feabecfcca234e
Ubuntu <ubuntu@dev.marcopeg.com>
2020-12-24 10:12:55 +0000

    Test with home ansible
    #24

--- ansible-files/run.sh
+++ ansible-files/run.sh
@@ -3,4 +3,7 @@
 sudo apt update -y
 sudo apt install -y ansible
 
+rm -rf /home/ubuntu/.ansible
+cp -r /home/ubuntu/code-server-ide/ansible-files/ansible/ /home/ubuntu/.ansible
+
 ansible-playbook code-server-ide.yml
