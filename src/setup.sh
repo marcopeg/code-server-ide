630d16e3ef42eef583e0e5c8b96480e7496447f6
Ubuntu <ubuntu@dev.marcopeg.com>
2020-12-29 04:22:21 +0000

    Skip nvm, dotnet, aws-cli
    #48

--- src/setup.sh
+++ src/setup.sh
@@ -10,9 +10,9 @@ source "${CODE_SERVER_SETUP_SRC_CWD}/setup-bashrc.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-code-server.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-docker.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-ctop.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-nvm.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-dotnet.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-aws-cli.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-nvm.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-dotnet.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-aws-cli.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-final.sh"
 
 # First boot:
