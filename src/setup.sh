abbb8eb908d64d2e3d10ca97cdc9b76120e7248c
Ubuntu <ubuntu@dev.marcopeg.com>
2020-12-29 07:29:54 +0000

    Do nothing while testing API

--- src/setup.sh
+++ src/setup.sh
@@ -3,17 +3,17 @@
 CODE_SERVER_SETUP_SRC_CWD="$(dirname "$0")"
 
 # Setup:
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-init.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-apt-get.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-env.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-bashrc.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-code-server.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-docker.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-ctop.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-init.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-apt-get.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-env.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-bashrc.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-code-server.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-docker.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-ctop.sh"
 # source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-nvm.sh"
 # source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-dotnet.sh"
 # source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-aws-cli.sh"
-source "${CODE_SERVER_SETUP_SRC_CWD}/setup-final.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-final.sh"
 
 # First boot:
-source "${CODE_SERVER_SETUP_SRC_CWD}/ec2-ubuntu-boot.sh"
+# source "${CODE_SERVER_SETUP_SRC_CWD}/ec2-ubuntu-boot.sh"
