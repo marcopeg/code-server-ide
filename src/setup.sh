5af6dc6ffd64c67a8e0a28863a5889154ca4a494
Ubuntu <ubuntu@dev.marcopeg.com>
2020-12-29 17:39:18 +0000

    Add code server

--- src/setup.sh
+++ src/setup.sh
@@ -7,17 +7,14 @@ source "${CODE_SERVER_SETUP_SRC_CWD}/setup-init.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-apt-get.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-env.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-bashrc.sh"
-# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-code-server.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-code-server.sh"
 source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-docker.sh"
-# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-ctop.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-ctop.sh"
 # source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-nvm.sh"
 # source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-dotnet.sh"
 # source "${CODE_SERVER_SETUP_SRC_CWD}/setup-software-aws-cli.sh"
-# source "${CODE_SERVER_SETUP_SRC_CWD}/setup-final.sh"
+source "${CODE_SERVER_SETUP_SRC_CWD}/setup-final.sh"
 
 # First boot:
-# source "${CODE_SERVER_SETUP_SRC_CWD}/ec2-ubuntu-boot.sh"
-
-# Just start nginx while testing
-docker run -d -p 80:80 nginx
+source "${CODE_SERVER_SETUP_SRC_CWD}/ec2-ubuntu-boot.sh"
 
